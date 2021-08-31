7b4a0a21c2866e6616ce3fb32c16a357693ea05e
m10hit <training@perennialsys.com>
2021-08-31 12:21:22 +0530

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import ClickCounter from './ClickCounter'
+import './styles.css'
+import Image from './react.png'
+import Logo from './React-icon.svg'
 export const App = () => {
-  return <h1>React TypeScript Webpack Starter Template</h1>;
-};
+  const name = 'Mohit'
+  return (
+    <>
+      <h1>
+        Hello React TypeScript Webpack Starter Template - {name}{' '}
+        {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={Image} alt="react-logo" width="300" height="200" />
+      <img src={Logo} alt="react-icon-svg" width="300" />
+      <ClickCounter />
+    </>
+  )
+}
